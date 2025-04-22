7c839614679de689f99dce8f1aecb3e766f5d8b2
arunvatty <arunv.inc@gmail.com>
2025-04-22 17:26:00 +0000

    Dockerized on Github Codespaces

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from app.api import router
 
 app = FastAPI()
 app.include_router(router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Log Classifier API is running", "endpoints": ["/predict"]}
